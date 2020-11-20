e9f7ef20ad6d6bf5afc8f0c3db69973468968f8b
AmjadRabby <glamarrabby@gmail.com>
2020-11-20 12:20:12 +0600

    almost done

---
+++ src/components/Home/Achievements/Achievements.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import './Achievements.css';
+import happy from '../../../resources/Illustration/happy-emojy3.png';
+import marketing from '../../../resources/Illustration/marketing@2x.png';
+import surface1 from '../../../resources/Illustration/surface1@2x.png';
+import transportation from '../../../resources/Illustration/transportation@2x.png';
+
+const Achievements = () => {
+    return (
+        <section className="achievement-container">
+                <div className="container"> 
+            <div className="row pt-5">
+                <div className="col-md-5 p-5 ">
+                <h2 className="font-weight-bold">Our Achievements</h2>
+                    <p className="text-muted"> <small> It is a long established fact that a reader will be distracted by
+                        the readable content of a page when looking at its layout. The
+                        point of using Lorem Ipsum is that it has a more-or-less normal
+                        distribution of letter</small></p>
+                </div>
+                <div className="col-md-7 ">
+                    <div className="row text-center">
+                        <div className="col-md-6 col-sm-12 p-2">
+                            <div className="happy-clients shadow achievement d-flex align-items-center pl-5">                               
+                                <div className="">
+                                    <img style={{width: '80px'}} src={happy} alt=""/>
+                                </div>
+                                <div className="">
+                                    <h3>700+</h3>
+                                    <p>Happy Clients</p>
+                                </div>                               
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-sm-12 p-2">
+                            <div className="projects-completed shadow achievement d-flex align-items-center pl-5">
+                                <div className="">
+                                    <img className="" src={marketing} alt=""/>
+                                </div>
+                                <div className="">
+                                    <h3>140+</h3>
+                                    <p>Projects completed</p>
+                                </div>                                
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-sm-12 p-2">
+                            <div className="crazy-minds shadow achievement d-flex align-items-center pl-5">
+                                <div className="">
+                                <img src={surface1} alt=""/>
+                                </div>
+                                <div className="">
+                                    <h3>25+</h3>
+                                    <p>Crazy Mind</p>
+                                </div>                                
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-sm-12 p-2 ">
+                            <div className="running-projects shadow achievement d-flex align-items-center pl-5">
+                                <div className="">
+                                <img src={transportation} alt=""/>
+                                </div>
+                                <div className="">
+                                    <h3>75+</h3>
+                                    <p>Running Projects</p>
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+        
+    );
+};
+
+export default Achievements;
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -3,6 +3,10 @@ import AboutUs from '../AboutUs/AboutUs';
 import Header from '../Header/Header';
 import HeaderContent from '../Header/HeaderContent';
 import Services from '../Services/Services';
+import Achievements from '../Achievements/Achievements';
+import TeamOrder from '../TeamOrder/TeamOrder';
+import GetOffers from '../GetOffer/GetOffers';
+import Footer from '../Footer/Footer';
 
 const Home = () => {
     return (
@@ -11,6 +15,10 @@ const Home = () => {
             <HeaderContent></HeaderContent>
             <Services></Services>
             <AboutUs></AboutUs>
+            <Achievements/>
+            <TeamOrder/>
+            <GetOffers/>
+            <Footer/>
         </div>
     );
 };
